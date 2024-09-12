c3c5977decad6ebb61b0f100ac8c793dcf18bd96
AhmadParkhan25 <ahmadparkhan5555@gmail.com>
2024-09-12 22:43:57 +0700

    login

---
+++ larect/resources/js/Pages/Login1.jsx
@@ -0,0 +1,23 @@
+// import "./styles.css";
+import './css/styles.css';
+import logo from "./img/logo.svg";
+
+export default function Login1(){
+  return(
+    <section className="page login-1">
+    <div className="login-1-background"></div>
+    <div className="login-1-card">
+      <img src={logo} />
+      <h2>Welcome back</h2>
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Sign In</button>
+      </form>
+      <footer>
+        Need an account? Sign up <a href="#">here</a>
+      </footer>
+    </div>
+  </section>
+  )
+}
